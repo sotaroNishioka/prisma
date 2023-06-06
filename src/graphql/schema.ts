c714569f1bd6f0d3a8d7971db63a1ccb936af614
sotaroNishioka <nishiokanandesuka@gmail.com>
2023-06-06 17:58:36 +0900

    graphqlのschemaを定義

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,15 +1,76 @@
 export const typeDefs = `
-  type Link {
-    id: String
-    title: String
-    description: String
-    url: String
-    category: String
-    imageUrl: String
-    users: [String]
+  enunm BookStatus {
+    FAVORITE
+    IN_PROGRESS
+    DONE
+    DELETED
+  }
+
+  enum CardStatus {
+    FAVORITE
+    IN_PROGRESS
+    DONE
+    DELETED
+  }
+
+  enum UserStatus {
+    NOT_AUTHENTICATED
+    AUTHENTICATED
+  }
+
+
+  type User {
+    id: ID!
+    status: UserStatus!
+    name: String!
+    email: String!
+    books: [Book]!
+    cards: [Card]!
+  }
+  
+  type Book {
+    id: ID!
+    name: String
+    stauts: BookStatus!
+    userId: ID!
+    user: User!
+    cards: [Card]!
+  }
+
+  type Card {
+    id: ID!
+    status: CardStatus!
+    english: String!
+    japanese: String!
+    memo: String
+    userId: ID!
+    user: User!
+    bookId: ID!
+    book: Book!
+  }
+
+  input BookInput {
+    id: ID
+    name: String
+    status: BookStatus
+    userId: ID!
+  }
+
+  input CardInput {
+    id: ID
+    status: CardStatus
+    english: String
+    japanese: String
+    memo: String
+    userId: ID!
+    bookId: ID
   }
 
   type Query {
-    links: [Link]!
+    user(id: ID!): User!
+    getBook(id: ID!): Book!
+    getCard(id: ID!): Card!
+    listBooks(filter: BookInput): [Book]!
+    listCards(filter: CardInput): [Card]!
   }
 `;
